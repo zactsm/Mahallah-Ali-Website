1baeb25f58f212ea35e8422f06aba5418dbe737f
zactsm <121001048+zactsm@users.noreply.github.com>
2024-01-14 23:10:21 +0800

    Add files via upload

---
+++ script2.js
@@ -0,0 +1,16 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "100%";
+}
+
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+}
+
+const navigation = document.querySelector(".primary-navigation");
+
+const navigationHeight = navigation.offsetHeight;
+
+document.documentElement.style.setProperty(
+  "--scroll-padding",
+  navigationHeight + "px"
+);
